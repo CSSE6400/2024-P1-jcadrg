0a3d5184640a675735799da764267ed7e5c969d6
Juan Carlos Gómez <jcadrg@gmail.com>
2024-02-22 08:27:15 +0000

    fixed typos

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -24,7 +24,7 @@ def get_todo(id):
     return jsonify({ 
         "id":id, 
         "title": "WatchCSSE6400Lecture", 
-        "description": "WatchtheCSSE6400lecture onECHO360forweek 1", 
+        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1", 
         "completed": True, 
         "deadline_at": "2023-02-27T00:00:00", 
         "created_at": "2023-02-20T00:00:00", 
@@ -35,20 +35,20 @@ def get_todo(id):
 def create_todo():
     return jsonify({
         "id":1,
-        "title": "WatchCSSE6400Lecture", 
-        "description": "WatchtheCSSE6400lecture onECHO360forweek 1", 
+        "title": "Watch CSSE6400 Lecture", 
+        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1", 
         "completed": True,
         "deadline_at": "2023-02-27T00:00:00",
         "created_at": "2023-02-20T00:00:00", 
         "updated_at": "2023-02-20T00:00:00"
-    },201)
+    })
 
 @api.route('/todos/<int:id>',methods=['PUT'])
 def update_todo(id):
     return jsonify({
         "id":id,
-        "title": "WatchCSSE6400Lecture", 
-        "description": "WatchtheCSSE6400lecture onECHO360forweek 1", 
+        "title": "Watch CSSE6400 Lecture", 
+        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1", 
         "completed": True, 
         "deadline_at": "2023-02-27T00:00:00",
         "created_at": "2023-02-20T00:00:00", 
@@ -59,8 +59,8 @@ def update_todo(id):
 def delete_todo(id):
     return jsonify({
         "id":id,
-        "title": "WatchCSSE6400Lecture",
-        "description": "WatchtheCSSE6400lecture onECHO360forweek 1",
+        "title": "Watch CSSE6400 Lecture",
+        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
         "completed": True,
         "deadline_at": "2023-02-27T00:00:00",
         "created_at": "2023-02-20T00:00:00",
